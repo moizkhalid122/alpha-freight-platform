5089261c79ed76aad57366e78a7178433769e557
moizkhalid122 <moizkhalid122@gmail.com>
2026-07-03 09:03:13 +0500

    Default GA measurement ID so tracking works before Vercel env is set

--- alpha-freight-modern/src/components/GoogleAnalytics.tsx
+++ alpha-freight-modern/src/components/GoogleAnalytics.tsx
@@ -1,10 +1,9 @@
 import Script from "next/script";
 
-const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+const GA_ID =
+  process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID ?? "G-E57VQK86NV";
 
 export default function GoogleAnalytics() {
-  if (!GA_ID) return null;
-
   return (
     <>
       <Script
